f3b15767e9d362b4ecdd4309ede349124609737c
mdshahadothosen2001 <mdshahadothosen2001@gmail.com>
2026-01-30 17:49:56 +0600

    chore: logo component clickable

--- src/components/atoms/Logo.tsx
+++ src/components/atoms/Logo.tsx
@@ -7,21 +7,24 @@ interface LogoProps {
 const Logo: React.FC<LogoProps> = ({ className = "" }) => {
   return (
     <div className={`flex items-center gap-3 ${className}`} style={{ height: 83 }}>
-      <div
-        style={{
-          width: "auto",
-          height: 83,
-          boxSizing: "border-box",
-          borderRadius: 6,
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          overflow: "hidden",
-          backgroundColor: "transparent",
-        }}
-      >
-        <img src="/logo.png" alt="TOTC logo" style={{ width: "auto", height: 83, objectFit: "cover" }} />
-      </div>
+      <a href="/" aria-label="Home" style={{ display: "inline-block", textDecoration: "none" }}>
+        <div
+          style={{
+            width: "auto",
+            height: 83,
+            boxSizing: "border-box",
+            borderRadius: 6,
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            overflow: "hidden",
+            backgroundColor: "transparent",
+            cursor: "pointer",
+          }}
+        >
+          <img src="/logo.png" alt="TOTC logo" style={{ width: "auto", height: 83, objectFit: "cover" }} />
+        </div>
+      </a>
     </div>
   );
 };
