94f8542ca9a12f85ab61e65798f7ce5513179680
mdshahadothosen2001 <mdshahadothosen2001@gmail.com>
2026-02-01 18:35:19 +0600

    feat: add navitems

---
+++ src/constants/NavItem.tsx
@@ -0,0 +1,7 @@
+export const navItems = [
+  { label: "Home", href: "/" },
+  { label: "Courses", href: "/course" },
+  { label: "Careers", href: "/search" },
+  { label: "Blog", href: "/blog" },
+  { label: "About Us", href: "/checkout" },
+];
