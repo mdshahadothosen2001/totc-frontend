79f3e0863adae3100d39953e5a12e61cd8d8f5c9
mdshahadothosen2001 <mdshahadothosen2001@gmail.com>
2026-02-01 20:58:18 +0600

    feat: small component for course details

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -1,18 +1,26 @@
-export { default as BaseButton } from "./button/BaseButton";
-export { default as PrimaryButton } from "./button/PrimaryButton";
-export { default as SecondaryButton } from "./button/SecondaryButton";
-export { default as OutlineButton } from "./button/OutlineButton";
-export { default as BoxButton } from "./button/BoxButton";
-export { default as GhostButton } from "./button/GhostButton";
-export { default as PasswordToggle } from "./PasswordToggle";
-export { default as Input } from "./Input";
-export { default as ImageContainer } from "./ImageContainer";
+// Basic Reusable component exports
 export { default as Card } from "./Card";
 export { default as Logo } from "./Logo";
+export { default as Input } from "./Input";
+export { default as Text } from "./text/Text";
 export { default as UserProfile } from "./UserProfile";
-export { default as UserAvatar } from "./userAvatar/UserAvatar";
-export { default as price } from "./price/Price";
+export { default as Price } from "./price/Price";
 export { default as PriceTag } from "./price/PriceTag";
 export { default as SeeAllLink } from "./SeeAllLink";
+export { default as PasswordToggle } from "./PasswordToggle";
+export { default as ImageContainer } from "./ImageContainer";
+export { default as BaseButton } from "./button/BaseButton";
+export { default as BoxButton } from "./button/BoxButton";
+export { default as OutlineButton } from "./button/OutlineButton";
+export { default as GhostButton } from "./button/GhostButton";
+export { default as PrimaryButton } from "./button/PrimaryButton";
+export { default as SecondaryButton } from "./button/SecondaryButton";
+export { default as UserAvatar } from "./userAvatar/UserAvatar";
+
+// Title components
 export { default as BlogSectionTitle } from "./title/BlogSectionTitle";
 export { default as CourseSectionTitle } from "./title/CourseSectionTitle";
+
+// Course Details components
+export { default as PriceDisplay } from "./courseDetails/PricingDisplay";
+
